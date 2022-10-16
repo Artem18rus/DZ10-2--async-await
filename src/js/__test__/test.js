b2cee0b924af2c5a3207fdbf109ad87bde1746c0
Artem18 <urart88@mail.ru>
2022-10-16 10:51:25 +0400

    3

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -2,14 +2,20 @@ import json from '../parser.js';
 import read from '../reader.js';
 import main from '../app.js';
 
-test('Function-ERR1', async () => {
+test('Function-OK', async () => {
   const data = await main();
-  try {
     await main();
-  }
-  catch (e) {
-    expect(e.name).toEqual('error');
-  }
+});
+
+test('Function-ERR', async () => {
+  async function main() {
+      const resultRead = await Promise.resolve(read());
+      const resultJson = await Promise.resolve(json(resultRead));
+      return JSON.parse(resultJson);
+  };
+  
+  const data = await main();
+  expect(data).toBe('error')
 });
 
 
