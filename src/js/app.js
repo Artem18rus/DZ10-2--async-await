0e6b9a7c2c914347cf32540e0624f5002adde219
Artem18 <urart88@mail.ru>
2022-10-16 10:13:49 +0400

    2

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,7 @@ import json from './parser.js';
 import read from './reader.js';
 
 export default async function main() {
-  try {
     const resultRead = await Promise.resolve(read());
     const resultJson = await Promise.resolve(json(resultRead));
     return JSON.parse(resultJson);
-  }
-  catch(e) {
-    return 'error';
-  }
 }
